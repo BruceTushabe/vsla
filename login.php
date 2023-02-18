fad9d4b9b634e37f8882f393cd5d1b7e3195ce98
Bruce Tushabe <brucetushabe@gmail.com>
2023-02-18 17:47:00 +0300

    Added the login files

--- login.php
+++ login.php
@@ -1,29 +1,25 @@
-
 <?php
 session_start();
-
-$host = "localhost";
-$username = "your_mysql_username";
-$password = "your_mysql_password";
-$database = "your_database_name";
-
-$conn = mysqli_connect($host, $username, $password, $database);
+include('config.php');
 
 if (isset($_POST['login'])) {
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
-    $password = md5($password);
 
-    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($conn, $query);
-
-    if (mysqli_num_rows($result) == 1) {
-        $_SESSION['username'] = $username;
-        header('location: dashboard.php');
+    if (empty($username)) {
+        $error = "Username is required";
+    } else if (empty($password)) {
+        $error = "Password is required";
     } else {
-            $error = "Invalid username or password!";
+        $query = "SELECT * FROM users WHERE username='$username' AND password='" . md5($password) . "'";
+        $result = mysqli_query($conn, $query);
+
+        if (mysqli_num_rows($result) == 1) {
+            $_SESSION['username'] = $username;
+            header('location: dashboard.php');
+        } else {
+            $error = "Invalid username or password";
         }
     }
-    
-    ?>
-    
+}
+?>
